157b41fc3f7b6a5c2727617af3c9a989a906a12e
Jacob Stern <jacob.stern@outlook.com>
2023-08-23 18:10:20 -0700

    Implement playback from queue

--- src-tauri/src/player/mod.rs
+++ src-tauri/src/player/mod.rs
@@ -1,14 +1,7 @@
-use std::{
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
-    thread,
-    time::Duration,
-};
+use std::{thread, time::Duration};
 
 use creek::{Decoder, ReadDiskStream, ReadStreamOptions, SymphoniaDecoder};
-use log::warn;
+use log::{info, warn};
 use rtrb::RingBuffer;
 use std::sync::mpsc;
 
@@ -31,20 +24,55 @@ pub enum ProcessToGuiMsg {
 }
 
 enum ManagerCommand {
-    StartPlayback(String),
+    StartPlayback(Vec<String>),
     Pause,
     PlaybackPos(usize),
+    PlaybackEnded,
     Buffering,
 }
 
+#[derive(Clone)]
+struct Queue<T> {
+    elements: Vec<T>,
+    index: usize,
+}
+
+impl<T> Queue<T> {
+    pub fn from_vec(elements: Vec<T>) -> Option<Queue<T>> {
+        if elements.is_empty() {
+            None
+        } else {
+            Some(Queue { elements, index: 0 })
+        }
+    }
+
+    pub fn current(&self) -> &T {
+        &self.elements[self.index]
+    }
+
+    pub fn go_next(self) -> Option<Queue<T>> {
+        if self.index + 1 < self.elements.len() {
+            Some(Queue {
+                index: self.index + 1,
+                elements: self.elements,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+const NUM_CACHE_BLOCKS: usize = 20;
+
+#[allow(unused)]
+const CACHE_SIZE: usize = NUM_CACHE_BLOCKS * SymphoniaDecoder::DEFAULT_BLOCK_SIZE;
+
 struct PlaybackManager {
-    stream: cpal::Stream,
+    _stream: cpal::Stream,
     to_process_tx: rtrb::Producer<GuiToProcessMsg>,
-    from_process_rx: rtrb::Consumer<ProcessToGuiMsg>,
-    command_tx: mpsc::Sender<ManagerCommand>,
     command_rx: mpsc::Receiver<ManagerCommand>,
     stream_frame_count: Option<usize>,
-    cache_size: usize,
+    queue: Option<Queue<String>>,
 }
 
 impl PlaybackManager {
@@ -52,98 +80,50 @@ impl PlaybackManager {
         command_tx: mpsc::Sender<ManagerCommand>,
         command_rx: mpsc::Receiver<ManagerCommand>,
     ) -> PlaybackManager {
-        let (to_gui_tx, from_process_rx) = RingBuffer::<ProcessToGuiMsg>::new(256);
+        let (to_gui_tx, mut from_process_rx) = RingBuffer::<ProcessToGuiMsg>::new(256);
         let (to_process_tx, from_gui_rx) = RingBuffer::<GuiToProcessMsg>::new(64);
         let cpal_stream = output::start_stream(to_gui_tx, from_gui_rx);
 
-        PlaybackManager {
-            stream: cpal_stream,
-            to_process_tx,
-            from_process_rx,
-            command_tx,
-            command_rx,
-            stream_frame_count: None,
-            cache_size: 0,
-        }
-    }
-
-    pub fn run(mut self) {
-        let mut from_process_rx = self.from_process_rx;
-
         thread::spawn({
-            let tx = self.command_tx.clone();
+            let tx = command_tx.clone();
             move || {
                 let mut failed_to_send = false;
                 while !failed_to_send {
                     while let Ok(msg) = from_process_rx.pop() {
-                        match msg {
-                            ProcessToGuiMsg::Buffering => {
-                                if tx.send(ManagerCommand::Buffering).is_err() {
-                                    failed_to_send = true;
-                                    break;
-                                }
-                            }
-                            ProcessToGuiMsg::PlaybackPos(pos) => {
-                                if tx.send(ManagerCommand::PlaybackPos(pos)).is_err() {
-                                    failed_to_send = true;
-                                    break;
-                                }
-                            }
-                            ProcessToGuiMsg::PlaybackEnded => {
-                                println!("Playback ended");
-                            }
+                        let result = tx.send(match msg {
+                            ProcessToGuiMsg::Buffering => ManagerCommand::Buffering,
+                            ProcessToGuiMsg::PlaybackPos(pos) => ManagerCommand::PlaybackPos(pos),
+                            ProcessToGuiMsg::PlaybackEnded => ManagerCommand::PlaybackEnded,
+                        });
+                        failed_to_send = result.is_err();
+                        if failed_to_send {
+                            break;
                         }
                     }
-                    thread::sleep(Duration::from_millis(10));
+                    if !failed_to_send {
+                        thread::sleep(Duration::from_millis(10));
+                    }
                 }
             }
         });
 
+        PlaybackManager {
+            _stream: cpal_stream,
+            to_process_tx,
+            command_rx,
+            stream_frame_count: None,
+            queue: None,
+        }
+    }
+
+    pub fn run(mut self) {
         while let Ok(msg) = self.command_rx.recv() {
             match msg {
-                ManagerCommand::StartPlayback(file_path) => {
-                    // Setup read stream -------------------------------------------------------------
-
-                    let opts = ReadStreamOptions {
-                        // The number of prefetch blocks in a cache block. This will cause a cache to be
-                        // used whenever the stream is seeked to a frame in the range:
-                        //
-                        // `[cache_start, cache_start + (num_cache_blocks * block_size))`
-                        //
-                        // If this is 0, then the cache is only used when seeked to exactly `cache_start`.
-                        num_cache_blocks: 20,
-
-                        // The maximum number of caches that can be active in this stream. Keep in mind each
-                        // cache uses some memory (but memory is only allocated when the cache is created).
-                        //
-                        // The default is `1`.
-                        num_caches: 2,
-                        ..Default::default()
-                    };
-
-                    self.cache_size = opts.num_cache_blocks * SymphoniaDecoder::DEFAULT_BLOCK_SIZE;
-
-                    // Open the read stream.
-                    let mut read_stream =
-                        ReadDiskStream::<SymphoniaDecoder>::new(file_path, 0, opts).unwrap();
-
-                    // Cache the start of the file into cache with index `0`.
-                    let _ = read_stream.cache(0, 0);
-
-                    // Tell the stream to seek to the beginning of file. This will also alert the stream to the existence
-                    // of the cache with index `0`.
-                    read_stream.seek(0, Default::default()).unwrap();
-
-                    // Wait until the buffer is filled before sending it to the process thread.
-                    // read_stream.block_until_ready().unwrap();
-
-                    // ------------------------------------------------------------------------------
-
-                    self.stream_frame_count = Some(read_stream.info().num_frames);
-
-                    self.to_process_tx
-                        .push(GuiToProcessMsg::StartPlayback(read_stream))
-                        .unwrap();
+                ManagerCommand::StartPlayback(file_paths) => {
+                    self.queue = Queue::from_vec(file_paths);
+                    if let Some(queue) = self.queue.as_ref() {
+                        self.start_stream(queue.current().to_owned());
+                    }
                 }
                 ManagerCommand::Pause => self
                     .to_process_tx
@@ -153,8 +133,56 @@ impl PlaybackManager {
                     }),
                 ManagerCommand::Buffering => {}
                 ManagerCommand::PlaybackPos(_pos) => {}
+                ManagerCommand::PlaybackEnded => {
+                    self.queue = self.queue.and_then(|queue| queue.go_next());
+                    if let Some(queue) = self.queue.as_ref() {
+                        self.start_stream(queue.current().to_owned());
+                    }
+                }
             }
         }
+    }
+
+    fn start_stream(&mut self, path: String) {
+        // Setup read stream -------------------------------------------------------------
+
+        let opts = ReadStreamOptions {
+            // The number of prefetch blocks in a cache block. This will cause a cache to be
+            // used whenever the stream is seeked to a frame in the range:
+            //
+            // `[cache_start, cache_start + (num_cache_blocks * block_size))`
+            //
+            // If this is 0, then the cache is only used when seeked to exactly `cache_start`.
+            num_cache_blocks: NUM_CACHE_BLOCKS,
+
+            // The maximum number of caches that can be active in this stream. Keep in mind each
+            // cache uses some memory (but memory is only allocated when the cache is created).
+            //
+            // The default is `1`.
+            num_caches: 2,
+            ..Default::default()
+        };
+
+        info!("Starting stream for {:?}", path);
+
+        // Open the read stream.
+        let mut read_stream = ReadDiskStream::<SymphoniaDecoder>::new(path, 0, opts).unwrap();
+
+        // Cache the start of the file into cache with index `0`.
+        let _ = read_stream.cache(0, 0);
+
+        // Tell the stream to seek to the beginning of file. This will also alert the stream to the existence
+        // of the cache with index `0`.
+        read_stream.seek(0, Default::default()).unwrap();
+
+        // Wait until the buffer is filled before sending it to the process thread.
+        // read_stream.block_until_ready().unwrap();
+
+        self.stream_frame_count = Some(read_stream.info().num_frames);
+
+        self.to_process_tx
+            .push(GuiToProcessMsg::StartPlayback(read_stream))
+            .unwrap();
     }
 }
 
@@ -169,15 +197,15 @@ impl Player {
         let (command_tx, rx) = mpsc::channel();
         thread::spawn({
             let tx = command_tx.clone();
+            // TODO: Fix up builder pattern for PlaybackManager
             move || PlaybackManager::new(tx, rx).run()
         });
         Player { command_tx }
     }
 
     pub fn start_playback(&mut self, file_paths: &[String]) {
-        let file_path = file_paths[0].to_owned();
         self.command_tx
-            .send(ManagerCommand::StartPlayback(file_path))
+            .send(ManagerCommand::StartPlayback(Vec::from(file_paths)))
             .unwrap_or_else(|_| {
                 warn!("Failed to send start playback command to the manager");
             })
