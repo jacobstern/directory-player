07fa79729af891916af8570b0f7b96d1294fdc11
Jacob Stern <jacob.stern@outlook.com>
2023-08-25 17:23:27 -0700

    Add seek bar component

--- src/player/ui/player-pane/index.tsx
+++ src/player/ui/player-pane/index.tsx
@@ -2,6 +2,7 @@ import "./player-pane.styles.css";
 import PauseButton from "./pause-button";
 import PlayButton from "./play-button";
 import VolumeSlider from "./volume-slider";
+import SeekBar from "./seek-bar";
 
 export default function PlayerPane() {
   return (
@@ -12,6 +13,7 @@ export default function PlayerPane() {
           <PauseButton />
           <VolumeSlider />
         </div>
+        <SeekBar />
       </div>
     </section>
   );
