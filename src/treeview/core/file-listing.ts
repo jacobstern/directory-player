633c940b72fdbf4b3bf598b2c2dc838cc6de0e6d
Jacob Stern <jacob.stern@outlook.com>
2023-12-10 20:21:58 -0800

    Move file listing to class

--- src/treeview/core/file-listing.ts
+++ src/treeview/core/file-listing.ts
@@ -36,27 +36,89 @@ export type ChangeListener = () => void;
 export type UnsubscribeFunction = VoidFunction;
 
 export interface FileListing {
+  readRootDir(path: string): Promise<void>;
   getRoot(): Root;
   addChangeListener(listener: ChangeListener): UnsubscribeFunction;
   expandDirectory(path: string): void;
   collapseDirectory(path: string): void;
-  /**
-   * Detach event listeners etc.
-   */
-  dispose(): void;
+}
+
+class FileListingImpl implements FileListing {
+  private root: Root = null;
+  private pubSub = new BasicPubSub();
+  private directoryReverseLookup = new Map<string, File>();
+
+  async readRootDir(path: string): Promise<void> {
+    const normalizedPath = await normalize(path);
+    const listing = await readDir(normalizedPath, { recursive: false });
+    this.directoryReverseLookup.clear();
+    for (const child of listing) {
+      if (child.children !== undefined) {
+        this.directoryReverseLookup.set(child.path, child);
+      }
+    }
+    this.root = {
+      path: normalizedPath,
+      children: listing,
+    };
+    this.pubSub.notify();
+  }
+
+  getRoot() {
+    return this.root;
+  }
+
+  addChangeListener(listener: ChangeListener): UnsubscribeFunction {
+    return this.pubSub.listen(listener);
+  }
+
+  async expandDirectory(path: string): Promise<void> {
+    const directory = this.directoryReverseLookup.get(path);
+    if (directory === undefined) {
+      throw new Error(`Failed to locate directory at "${path}"`);
+    }
+    if (directory.isExpanded) return;
+    directory.isExpanded = true;
+    const children = await readDir(path, { recursive: false });
+    directory.children = children;
+    for (const child of children) {
+      if (child.children !== undefined) {
+        this.directoryReverseLookup.set(child.path, child);
+      }
+    }
+    this.pubSub.notify();
+  }
+
+  async collapseDirectory(path: string): Promise<void> {
+    const directory = this.directoryReverseLookup.get(path);
+    if (directory === undefined) {
+      throw new Error(`Failed to locate directory at "${path}"`);
+    }
+    if (!directory.isExpanded || !directory.children) return;
+    for (const child of directory.children) {
+      this.directoryReverseLookup.delete(child.path);
+    }
+    directory.children = [];
+    directory.isExpanded = false;
+    this.pubSub.notify();
+  }
 }
 
 export async function initFileListing(): Promise<FileListing> {
-  let root: Root = null;
   let isDialogOpen = false;
-  const pubSub = new BasicPubSub();
-  const directoryReverseLookup = new Map<string, File>();
 
   const handleMenuEvent = (menuItemId: string) => {
     if (menuItemId === "open") {
       openDialog();
     }
   };
+  // TODO: Move initialization and menu handling logic out of this file
+  await listen("app://menu-event", (event) => {
+    const menuItemId = z.string().parse(event.payload);
+    handleMenuEvent(menuItemId);
+  });
+  const fileListing = new FileListingImpl();
+
   const openDialog = async (): Promise<void> => {
     if (isDialogOpen) {
       return;
@@ -72,7 +134,7 @@ export async function initFileListing(): Promise<FileListing> {
     if (typeof path === "string") {
       let success = false;
       try {
-        await readRootDir(path);
+        await fileListing.readRootDir(path);
         success = true;
       } catch {
         // TODO: Surface error
@@ -80,23 +142,8 @@ export async function initFileListing(): Promise<FileListing> {
 
       if (success) {
         syncStorage.set(DIRECTORY_STORAGE_KEY, path);
-        pubSub.notify();
       }
     }
-  };
-  const readRootDir = async (path: string): Promise<void> => {
-    const normalizedPath = await normalize(path);
-    const listing = await readDir(normalizedPath, { recursive: false });
-    directoryReverseLookup.clear();
-    for (const child of listing) {
-      if (child.children !== undefined) {
-        directoryReverseLookup.set(child.path, child);
-      }
-    }
-    root = {
-      path: normalizedPath,
-      children: listing,
-    };
   };
   const persistedDir = syncStorage.getWithSchema(
     DIRECTORY_STORAGE_KEY,
@@ -104,55 +151,12 @@ export async function initFileListing(): Promise<FileListing> {
   );
   if (persistedDir !== null) {
     try {
-      await readRootDir(persistedDir);
+      await fileListing.readRootDir(persistedDir);
     } catch (e) {
       warn(`Failed to load persisted root with "${e}"`);
       syncStorage.set(DIRECTORY_STORAGE_KEY, null);
     }
   }
-  const unlistenMenuEvent = await listen("app://menu-event", (event) => {
-    const menuItemId = z.string().parse(event.payload);
-    handleMenuEvent(menuItemId);
-  });
 
-  return {
-    getRoot() {
-      return root;
-    },
-    addChangeListener(listener) {
-      return pubSub.listen(listener);
-    },
-    async expandDirectory(path) {
-      const directory = directoryReverseLookup.get(path);
-      if (directory === undefined) {
-        throw new Error(`Failed to locate directory at "${path}"`);
-      }
-      if (directory.isExpanded) return;
-      directory.isExpanded = true;
-      const children = await readDir(path, { recursive: false });
-      directory.children = children;
-      for (const child of children) {
-        if (child.children !== undefined) {
-          directoryReverseLookup.set(child.path, child);
-        }
-      }
-      pubSub.notify();
-    },
-    async collapseDirectory(path) {
-      const directory = directoryReverseLookup.get(path);
-      if (directory === undefined) {
-        throw new Error(`Failed to locate directory at "${path}"`);
-      }
-      if (!directory.isExpanded || !directory.children) return;
-      for (const child of directory.children) {
-        directoryReverseLookup.delete(child.path);
-      }
-      directory.children = [];
-      directory.isExpanded = false;
-      pubSub.notify();
-    },
-    dispose() {
-      unlistenMenuEvent();
-    },
-  };
+  return fileListing;
 }
